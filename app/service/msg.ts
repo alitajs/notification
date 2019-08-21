d2ca2dd240785fdf5ed517f5e63a07ee8affff6e
imhele <work@imhele.com>
2019-08-21 16:16:58 +0800

    fetch messages

--- app/service/msg.ts
+++ app/service/msg.ts
@@ -1,7 +1,9 @@
 import { DefineMsgrepo } from '@/model/msgrepo';
+import { DefineMsgsync } from '@/model/msgsync';
 import { validateAttr } from '@/utils';
 // import { NotFound } from '@/utils/errorcode';
 import { Service } from 'egg';
+import { Op } from 'sequelize';
 
 /**
  * Service of message
@@ -11,6 +13,34 @@ export default class MsgService extends Service {
   static RedisKey = {
     MsgId: (chatId: string) => `msgid:${chatId}`,
   };
+
+  /**
+   * use `afterMsgId` instead of `offset`
+   */
+  public getChatHistoryMsgs(chatId: string, afterMsgId: number, limit?: number) {
+    const attrs = validateAttr(DefineMsgrepo, { chatId, msgId: afterMsgId });
+    return this.ctx.model.Msgrepo.findAll({
+      limit,
+      where: {
+        chatId: attrs.chatId,
+        msgId: { [Op.gt]: attrs.msgId },
+      },
+    });
+  }
+
+  /**
+   * use `afterTime` instead of `offset`
+   */
+  public getChatHistoryMsgsByTime(chatId: string, afterTime: number, limit?: number) {
+    const attrs = validateAttr(DefineMsgrepo, { chatId, createTime: afterTime });
+    return this.ctx.model.Msgrepo.findAll({
+      limit,
+      where: {
+        chatId: attrs.chatId,
+        createTime: { [Op.gt]: attrs.createTime },
+      },
+    });
+  }
 
   public async getNextMsgId(chatId: string) {
     chatId = validateAttr(DefineMsgrepo, { chatId }).chatId;
@@ -25,6 +55,20 @@ export default class MsgService extends Service {
       nextMsgId = await this.app.redis.incrsetnx([chatId], lastMsgId, MsgService.MsgIdRedisExpire);
     }
     return nextMsgId;
+  }
+
+  /**
+   * use `afterTime` instead of `offset`
+   */
+  public getRecentMsgs(recipientId: string, afterTime?: number, limit?: number) {
+    const attrs = validateAttr(DefineMsgsync, { createTime: afterTime || 0, recipientId });
+    return this.ctx.model.Msgsync.findAll({
+      limit,
+      where: {
+        recipientId: attrs.recipientId,
+        createTime: { [Op.gt]: attrs.createTime },
+      },
+    });
   }
 
   private getNextMsgIdFromRedis(chatId: string) {
